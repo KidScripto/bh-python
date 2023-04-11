7a93b9c458201a1934b3751612a26f95df2285df
Drew Meylan <kidscripto@proton.me>
2023-04-11 14:29:47 -0400

    Updated with argparse. Intermediary form

--- email_scraper.py
+++ email_scraper.py
@@ -1,14 +1,24 @@
+import argparse
 import csv
 import requests
+
+
 from bs4 import BeautifulSoup
 
-def search_emails(domain):
+
+# Argparser
+parser = argparse.ArgumentParser(description="Receive arguments regarding target domain(s) and directory to place output csv file.")
+parser.add_argument("-d", "--Domain", help="Target domain", type=str, required=False)
+parser.add_argument("-of", "--OutputFile", help="Output director", type=str, required=True)
+parser.add_argument("-if", "--InputFile", help="Pass a worldist file of different domains to try", required=False)
+args = parser.parse_args()
+# Function Definitions
+def search_domain(args.Domain):
     # Use Google search to find email addresses associated with the domain
-    url = f"https://www.google.com/search?q=%40{domain}&oq=%40{domain}&aqs=chrome.0.35i39l2j0l4j46j69i60.9157j1j7&sourceid=chrome&ie=UTF-8"
+    url = f"https://www.google.com/search?q=%40{args.Domain}&oq=%40{args.Domain}&aqs=chrome.0.35i39l2j0l4j46j69i60.9157j1j7&sourceid=chrome&ie=UTF-8"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     links = soup.find_all("a")
-    
     # Extract the email addresses from the search results
     emails = []
     for link in links:
@@ -17,20 +27,35 @@ def search_emails(domain):
             emails.append(email.split(":")[1])
     return emails
 
-def export_to_csv(emails, domain):
+def export_to_csv(emails, args.Domain):
     # Export the email addresses to a CSV file
-    with open(f"{domain}.csv", "w", newline="") as csvfile:
+    with open(f"{args.Domain}.csv", "w", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(["Email"])
         for email in emails:
             csvwriter.writerow([email])
 
-if __name__ == "__main__":
+def search_list(args.InputFile):
+    with open(args.InputFile, "r", newline="") as file:
+        for line in file:
+            url = f"https://www.google.com/search?q=%40{line}&oq=%40{line}&aqs=chrome.0.35i39l2j0l4j46j69i60.9157j1j7&sourceid=chrome&ie=UTF-8"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, "html.parser")
+            links = soup.find_all("a")
+            emails = []
+            for link in links:
+                email = link.get("href")
+                if email and "mailto:" in email:
+                    emails.append(email.split(":")[1])
+                return emails
+            
+# __main__
+if __name__ == "__main__":  ### Implement flow control to execute different functions depending on which input parameters were supplied
     import sys
     if len(sys.argv) < 2:
         print("Please specify a domain name")
         sys.exit()
     domain = sys.argv[1]
-    emails = search_emails(domain)
-    export_to_csv(emails, domain)
+    emails = search_domain(args.Domain)
+    export_to_csv(emails, args.Domain)
     print(f"{len(emails)} email addresses associated with {domain} have been exported to {domain}.csv")
