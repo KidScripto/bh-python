d83d2019931c4595a9a95efe0f1a948e299ffca2
Drew Meylan <kidscripto@pop-os.localdomain>
2023-04-10 21:28:40 -0400

    Parameters now handled with argparse module.

--- TCP_client.py
+++ TCP_client.py
@@ -2,40 +2,29 @@
 Author: KidScripto
 Date: 04-09-2023
 """
-
+#! ./bhpy/bin/ python3.10
 
 import socket
 import sys, getopt
+import argparse
 
+#Arguments Definition
+parser = argparse.ArgumentParser(description="Get Hostname, Port number and IP address of the target server.")
+parser.add_argument('-n', '--hostname', help='Target Hostname', required=False, type=str)
+parser.add_argument('-p', '--port', help='Target port', required=True, type=int)
+parser.add_argument('-i', '--ip_address', help='Target IP address', required=True, type=str)
+args = parser.parse_args()
 
-arg_list = sys.argv[1:]
-
-try:
-    opts, args = getopt.getopt(arg_list, "H:P:I:")
-except getopt.GetoptError as err:
-    print(f"The following error was encountered during execution:\n{err}")
-    exit()
-
-for opt, arg in opts:
-    if opt in ['-H']:  #Exception handling: Should not be able to cast to INT
-        host = arg
-    elif opt in ['-P']:
-        try: 
-            port = int(arg)
-        except:
-            print("The following error was encountered during execution:\nCannot cast -P value to int. Please enter a valid port number.")
-            exit()
-    elif opt in ['-I']:
-        IP = arg     #Exception handling: Should not be able to cast to INT
-
+#Argument Validation
+##
 #Create socket object
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 #Connect the Client
-client.connect((IP,port))
+client.connect((args.ip_address,int(args.port)))
 
 ##Send some data
-client.send((f"GET / HTTP/1.1\r\nHost: {host}\r\n\r\n".encode("utf-8")))
+client.send((f"GET / HTTP/1.1\r\nHost: {args.hostname}\r\n\r\n".encode("utf-8")))
 
 ###Receive some data
 response = client.recv(4096)
